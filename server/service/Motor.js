8ffce4accc3c1b2e99adbab7e3b412069bc6cf49
Leonti Bielski <prishelec@gmail.com>
2017-06-11 00:02:15 +1000

    Try to disable the motor

--- server/service/Motor.js
+++ server/service/Motor.js
@@ -4,13 +4,12 @@ class Motor {
         this.motor = require('motor-l298n')
 
         // in1Pin, in2Pin, enable1Pin, in3Pin, in4Pin, enable2Pin
-        this.l298n = this.motor.setup(6, 13, 5, 19, 26, 21);
+    //    this.l298n = this.motor.setup(6, 13, 5, 19, 26, 21);
 
         console.log('Creating l298n', this.l298n);
     }
 
     forward = speed => {
-        console.log(this.l298n)
         this.l298n.setSpeed(this.motor.LEFT, speed)
         this.l298n.setSpeed(this.motor.RIGHT, speed)
 
