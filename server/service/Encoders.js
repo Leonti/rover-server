2fadff79d36144b0d5155b3106dd5be509cdcac3
Leonti <prishelec@gmail.com>
2018-02-18 13:23:16 +1100

    Delay encoders initialization, don't fail on error

--- server/service/Encoders.js
+++ server/service/Encoders.js
@@ -4,22 +4,28 @@ class Encoders {
     onRightCallbacks = []
 
     constructor() {
-      const wpi = require('wiring-pi')
-      wpi.setup('gpio')
+      setTimeout(() => {
+        try {
+          const wpi = require('wiring-pi')
+          wpi.setup('gpio')
 
-      // gpio 22 - right
-      wpi.pinMode(22, wpi.INPUT);
-      wpi.pullUpDnControl(22, wpi.PUD_UP);
-      wpi.wiringPiISR(22, wpi.INT_EDGE_FALLING, () => {
-          this.onRightCallbacks.forEach(c => c())
-      });
+          // gpio 22 - right
+          wpi.pinMode(22, wpi.INPUT);
+          wpi.pullUpDnControl(22, wpi.PUD_UP);
+          wpi.wiringPiISR(22, wpi.INT_EDGE_FALLING, () => {
+              this.onRightCallbacks.forEach(c => c())
+          });
 
-      // gpio 23 - left
-      wpi.pinMode(23, wpi.INPUT);
-      wpi.pullUpDnControl(23, wpi.PUD_UP);
-      wpi.wiringPiISR(23, wpi.INT_EDGE_FALLING, () => {
-          this.onLeftCallbacks.forEach(c => c())
-      });
+          // gpio 23 - left
+          wpi.pinMode(23, wpi.INPUT);
+          wpi.pullUpDnControl(23, wpi.PUD_UP);
+          wpi.wiringPiISR(23, wpi.INT_EDGE_FALLING, () => {
+              this.onLeftCallbacks.forEach(c => c())
+          });
+        } catch (e) {
+          console.error('Failed to initialize encoders', e)
+        }
+      }, 5000)
     }
 
     onLeftTick = onLeft => this.onLeftCallbacks.push(onLeft)
